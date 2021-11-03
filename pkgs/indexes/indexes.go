f65cdbf3a95253ccf74f953ed12876535f3bb83e
Antoine Mercadal <amercadal@paloaltonetworks.com>
2021-11-03 10:50:01 -0700

    pkgs/api: added index ensure system

---
+++ pkgs/indexes/indexes.go
@@ -0,0 +1,93 @@
+package indexes
+
+import (
+	"strings"
+
+	"github.com/globalsign/mgo"
+	"go.aporeto.io/elemental"
+	"go.aporeto.io/manipulate"
+	"go.aporeto.io/manipulate/manipmongo"
+	"go.uber.org/zap"
+)
+
+// Ensure ensures the indexes declared in the specs are aligned.
+func Ensure(m manipulate.Manipulator, model elemental.ModelManager, packageName string) (err error) {
+
+	indexes := GetIndexes(packageName, model)
+
+	for ident, mIndexes := range indexes {
+		if err = manipmongo.EnsureIndex(m, ident, mIndexes...); err != nil {
+			zap.L().Warn("Unable to create index", zap.Error(err))
+		}
+	}
+
+	return nil
+}
+
+// GetIndexes returns all the indexes for all the identity in the model
+func GetIndexes(packageName string, model elemental.ModelManager) (mIndexes map[elemental.Identity][]mgo.Index) {
+
+	var indexes [][]string
+
+	mIndexes = map[elemental.Identity][]mgo.Index{}
+
+	for _, ident := range model.AllIdentities() {
+
+		if ident.Package != packageName {
+			continue
+		}
+
+		indexes = model.Indexes(ident)
+		if len(indexes) == 0 {
+			continue
+		}
+
+		iName := "index_" + ident.Name + "_"
+
+		for i := range indexes {
+
+			idx := mgo.Index{}
+
+			piName := iName
+			var hashedApplied bool
+
+			for _, name := range indexes[i] {
+
+				if hashedApplied {
+					panic("hashed index must not be a compound index")
+				}
+
+				switch name {
+
+				case ":shard":
+					piName = "shard_" + iName
+
+				case ":unique":
+					idx.Unique = true
+
+				default:
+
+					name = strings.ToLower(name)
+					if attSpec, ok := model.Identifiable(ident).(elemental.AttributeSpecifiable); ok {
+						if bsonName := attSpec.SpecificationForAttribute(name).BSONFieldName; bsonName != "" {
+							name = bsonName
+						}
+					}
+
+					idx.Key = append(idx.Key, name)
+
+					if strings.HasPrefix(name, "$hashed:") {
+						hashedApplied = true
+					}
+				}
+			}
+
+			idx.Name = piName + strings.Join(idx.Key, "_")
+			idx.Background = true
+
+			mIndexes[ident] = append(mIndexes[ident], idx)
+		}
+	}
+
+	return mIndexes
+}
---
+++ pkgs/indexes/indexes_test.go
@@ -0,0 +1,76 @@
+package indexes
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/globalsign/mgo"
+	"go.aporeto.io/a3s/pkgs/api"
+	"go.aporeto.io/elemental"
+)
+
+func TestGetIndexes(t *testing.T) {
+	type args struct {
+		packageName string
+		identity    elemental.Identity
+		model       elemental.ModelManager
+	}
+	tests := []struct {
+		name         string
+		args         args
+		wantMIndexes map[elemental.Identity][]mgo.Index
+	}{
+		{
+			name: "all indexes",
+			args: args{
+				packageName: "a3s",
+				identity:    api.AuthorizationIdentity,
+				model:       api.Manager(),
+			},
+			wantMIndexes: map[elemental.Identity][]mgo.Index{
+				api.AuthorizationIdentity: {
+					{
+						Name:       "index_authorization_namespace_flattenedsubject_disabled",
+						Key:        []string{"namespace", "flattenedsubject", "disabled"},
+						Background: true,
+					},
+					{
+						Name:       "index_authorization_namespace_flattenedsubject_propagate",
+						Key:        []string{"namespace", "flattenedsubject", "propagate"},
+						Background: true,
+					},
+					{
+						Name:       "shard_index_authorization_zone_zhash",
+						Key:        []string{"zone", "zhash"},
+						Background: true,
+						Unique:     true,
+					},
+					{
+						Name:       "index_authorization_namespace",
+						Key:        []string{"namespace"},
+						Background: true,
+					},
+					{
+						Name:       "index_authorization_namespace__id",
+						Key:        []string{"namespace", "_id"},
+						Background: true,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if gotMIndexes := GetIndexes(tt.args.packageName, tt.args.model); !reflect.DeepEqual(gotMIndexes[tt.args.identity], tt.wantMIndexes[tt.args.identity]) {
+				t.Errorf("GetIndexes()\n"+
+					"EXPECTED:\n"+
+					"%+v\n"+
+					"ACTUAL:\n"+
+					"%+v\n",
+					tt.wantMIndexes[tt.args.identity],
+					gotMIndexes[tt.args.identity])
+			}
+		})
+	}
+}
