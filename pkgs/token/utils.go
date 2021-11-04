e62dab7f10013b47fe6b16e54048c69295c9d0b4
Antoine Mercadal <amercadal@paloaltonetworks.com>
2021-11-04 11:45:18 -0700

    lint: added missing doc string

--- pkgs/token/utils.go
+++ pkgs/token/utils.go
@@ -5,6 +5,8 @@ import (
 	"go.aporeto.io/elemental"
 )
 
+// FromRequest retrieves the token from the given elemental.Request
+// first looking at the cookie x-a3s-token, then the request.Password.
 func FromRequest(req *elemental.Request) string {
 	if hreq := req.HTTPRequest(); hreq != nil {
 		if cookie, err := hreq.Cookie("x-a3s-token"); err == nil {
@@ -14,6 +16,8 @@ func FromRequest(req *elemental.Request) string {
 	return req.Password
 }
 
+// FromSession retrieves the token from the given bahamut.Session
+// first looking at the cookie x-a3s-token, then the session.Token(.
 func FromSession(session bahamut.Session) string {
 	if cookie, err := session.Cookie("x-a3s-token"); err == nil {
 		return cookie.Value
