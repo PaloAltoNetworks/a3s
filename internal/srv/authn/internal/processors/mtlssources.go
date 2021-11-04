1da4d4c74bc72760cdbd163beee3f67dfb8118a9
Antoine Mercadal <amercadal@paloaltonetworks.com>
2021-11-04 14:17:06 -0700

    new/authn: added processor for mtls source

---
+++ internal/srv/authn/internal/processors/mtlssources.go
@@ -0,0 +1,50 @@
+package processors
+
+import (
+	"go.aporeto.io/a3s/pkgs/api"
+	"go.aporeto.io/a3s/pkgs/crud"
+	"go.aporeto.io/bahamut"
+	"go.aporeto.io/manipulate"
+)
+
+// A MTLSSourcesProcessor is a bahamut processor for MTLSSource.
+type MTLSSourcesProcessor struct {
+	manipulator manipulate.Manipulator
+}
+
+// NewMTLSSourcesProcessor returns a new MTLSSourcesProcessor.
+func NewMTLSSourcesProcessor(manipulator manipulate.Manipulator) *MTLSSourcesProcessor {
+	return &MTLSSourcesProcessor{
+		manipulator: manipulator,
+	}
+}
+
+// ProcessCreate handles the creates requests for MTLSSource.
+func (p *MTLSSourcesProcessor) ProcessCreate(bctx bahamut.Context) error {
+	return crud.Create(bctx, p.manipulator, bctx.InputData().(*api.MTLSSource))
+}
+
+// ProcessRetrieveMany handles the retrieve many requests for MTLSSource.
+func (p *MTLSSourcesProcessor) ProcessRetrieveMany(bctx bahamut.Context) error {
+	return crud.RetrieveMany(bctx, p.manipulator, &api.MTLSSourcesList{})
+}
+
+// ProcessRetrieve handles the retrieve requests for MTLSSource.
+func (p *MTLSSourcesProcessor) ProcessRetrieve(bctx bahamut.Context) error {
+	return crud.Retrieve(bctx, p.manipulator, api.NewMTLSSource())
+}
+
+// ProcessUpdate handles the update requests for MTLSSource.
+func (p *MTLSSourcesProcessor) ProcessUpdate(bctx bahamut.Context) error {
+	return crud.Update(bctx, p.manipulator, bctx.InputData().(*api.MTLSSource))
+}
+
+// ProcessDelete handles the delete requests for MTLSSource.
+func (p *MTLSSourcesProcessor) ProcessDelete(bctx bahamut.Context) error {
+	return crud.Delete(bctx, p.manipulator, api.NewMTLSSource())
+}
+
+// ProcessInfo handles the info request for MTLSSource.
+func (p *MTLSSourcesProcessor) ProcessInfo(bctx bahamut.Context) error {
+	return crud.Info(bctx, p.manipulator, api.MTLSSourceIdentity)
+}
