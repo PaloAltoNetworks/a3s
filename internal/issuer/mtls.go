28a585917424cc8152f3f1d0d1f5bdcc05da8278
Antoine Mercadal <amercadal@paloaltonetworks.com>
2021-11-09 11:54:02 -0800

    actually align

--- internal/issuer/mtls.go
+++ internal/issuer/mtls.go
@@ -12,10 +12,8 @@ import (
 
 // MTLSIssuer issues IdentityToken from a TLS certificate.
 type MTLSIssuer struct {
-	signerFingerprints []string
-	token              *token.IdentityToken
-	certificate        *x509.Certificate
-	source             *api.MTLSSource
+	token  *token.IdentityToken
+	source *api.MTLSSource
 }
 
 // NewMTLSIssuer returns a new MTLSIssuer.
@@ -32,12 +30,12 @@ func NewMTLSIssuer(source *api.MTLSSource) *MTLSIssuer {
 }
 
 // FromCertificate prepares the issuer according to the provided x509.Certificate.
-func (c *MTLSIssuer) FromCertificate(certificate *x509.Certificate) error {
+func (c *MTLSIssuer) FromCertificate(cert *x509.Certificate) error {
 
 	pool := x509.NewCertPool()
 	pool.AppendCertsFromPEM([]byte(c.source.CertificateAuthority))
 
-	chains, err := certificate.Verify(x509.VerifyOptions{
+	chains, err := cert.Verify(x509.VerifyOptions{
 		Roots:     pool,
 		KeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
 	})
@@ -45,90 +43,89 @@ func (c *MTLSIssuer) FromCertificate(certificate *x509.Certificate) error {
 		return fmt.Errorf("unable Verify certificate: %w", err)
 	}
 
+	var fingerprints []string
 	for _, chain := range chains {
 		for _, cert := range chain {
-			c.signerFingerprints = append(
-				c.signerFingerprints,
+			fingerprints = append(
+				fingerprints,
 				fmt.Sprintf("%02X", sha1.Sum(cert.Raw)),
 			)
 		}
 	}
 
-	c.certificate = certificate
+	if v := cert.Subject.CommonName; v != "" {
+		c.token.Identity = append(c.token.Identity, fmt.Sprintf("commonname=%s", v))
+	}
+
+	if v := cert.SerialNumber; v != nil {
+		c.token.Identity = append(c.token.Identity, fmt.Sprintf("serialnumber=%s", v))
+	}
+
+	if vs := cert.Subject.Country; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("country=%s", v))
+		}
+	}
+
+	if vs := cert.Subject.Locality; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("locality=%s", v))
+		}
+	}
+
+	if vs := cert.Subject.Organization; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("organization=%s", v))
+		}
+	}
+
+	if vs := cert.Subject.OrganizationalUnit; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("organizationalunit=%s", v))
+		}
+	}
+
+	if vs := cert.Subject.PostalCode; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("postalcode=%s", v))
+		}
+	}
+
+	if vs := cert.Subject.Province; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("province=%s", v))
+		}
+	}
+
+	if vs := cert.Subject.StreetAddress; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("streetaddress=%s", v))
+		}
+	}
+
+	if vs := cert.EmailAddresses; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("email=%s", v))
+		}
+	}
+
+	if vs := cert.DNSNames; len(vs) != 0 {
+		for _, v := range vs {
+			c.token.Identity = append(c.token.Identity, fmt.Sprintf("dnsname=%s", v))
+		}
+	}
+
+	if len(fingerprints) > 0 {
+		// if > 0 it is guaranteed to have at least 2 items.
+		c.token.Identity = append(c.token.Identity, fmt.Sprintf("fingerprint=%s", fingerprints[0]))
+		c.token.Identity = append(c.token.Identity, fmt.Sprintf("issuerchain=%s", strings.Join(fingerprints[1:], ",")))
+	}
 
 	return nil
 }
 
 // Issue issues the token.IdentityToken derived from the the user certificate.
 func (c *MTLSIssuer) Issue() *token.IdentityToken {
-
-	if v := c.certificate.Subject.CommonName; v != "" {
-		c.token.Identity = append(c.token.Identity, fmt.Sprintf("commonname=%s", v))
-	}
-
-	if v := c.certificate.SerialNumber; v != nil {
-		c.token.Identity = append(c.token.Identity, fmt.Sprintf("serialnumber=%s", v))
-	}
-
-	if vs := c.certificate.Subject.Country; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("country=%s", v))
-		}
-	}
-
-	if vs := c.certificate.Subject.Locality; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("locality=%s", v))
-		}
-	}
-
-	if vs := c.certificate.Subject.Organization; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("organization=%s", v))
-		}
-	}
-
-	if vs := c.certificate.Subject.OrganizationalUnit; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("organizationalunit=%s", v))
-		}
-	}
-
-	if vs := c.certificate.Subject.PostalCode; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("postalcode=%s", v))
-		}
-	}
-
-	if vs := c.certificate.Subject.Province; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("province=%s", v))
-		}
-	}
-
-	if vs := c.certificate.Subject.StreetAddress; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("streetaddress=%s", v))
-		}
-	}
-
-	if vs := c.certificate.EmailAddresses; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("email=%s", v))
-		}
-	}
-
-	if vs := c.certificate.DNSNames; len(vs) != 0 {
-		for _, v := range vs {
-			c.token.Identity = append(c.token.Identity, fmt.Sprintf("dnsname=%s", v))
-		}
-	}
-
-	if len(c.signerFingerprints) > 0 {
-		// if > 0 it is guaranteed to have at least 2 items.
-		c.token.Identity = append(c.token.Identity, fmt.Sprintf("fingerprint=%s", c.signerFingerprints[0]))
-		c.token.Identity = append(c.token.Identity, fmt.Sprintf("issuerchain=%s", strings.Join(c.signerFingerprints[1:], ",")))
-	}
 
 	return c.token
 }
