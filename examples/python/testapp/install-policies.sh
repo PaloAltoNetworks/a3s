250c8dc4efba885e662a1d894826a4974137522a
Antoine Mercadal <amercadal@paloaltonetworks.com>
2021-11-15 09:18:43 -0800

    testapp: better install-policies script

--- examples/python/testapp/install-policies.sh
+++ examples/python/testapp/install-policies.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+function die() {
+	echo "Error: $1"
+	exit 1
+}
+
 mkdir -p certs
 export TLSGEN_OUT="./certs"
 export TLSGEN_FORCE="true"
@@ -22,18 +27,30 @@ export A3SCTL_API="https://127.0.0.1:44443"
 export A3SCTL_API_SKIP_VERIFY="true"
 
 echo
+echo "* Retrieving an admin token"
+A3SCTL_TOKEN="$(
+	a3sctl auth mtls \
+		--cert ../../../dev/.data/certificates/user-cert.pem \
+		--key ../../../dev/.data/certificates/user-key.pem \
+		--source-name root --source-namespace /
+)"
+export A3SCTL_TOKEN
+
+echo
 echo "* Deleting existing /testapp namespace"
 a3sctl api delete namespace "/testapp" -n /
 
 echo
 echo "* Creating /testapp namespace"
-a3sctl api create namespace --name "testapp" -n "/"
+a3sctl api create namespace --name "testapp" -n "/" ||
+	die "unable to create /testapp namespace"
 
 echo
 echo "* Creating mtlssource"
 a3sctl api create mtlssource -n "/testapp" \
 	--name "default" \
-	--certificate-authority "$(cat certs/a3s-test-authority-cert.pem)"
+	--certificate-authority "$(cat certs/a3s-test-authority-cert.pem)" ||
+	die "unable to create mtls resource"
 
 echo
 echo "* Creating authorization for /secret"
@@ -48,7 +65,8 @@ a3sctl api create authorization -n "/testapp" \
 			"commonname=john"
 		]
 	]' \
-	--permissions '["/secret,GET"]'
+	--permissions '["/secret,GET"]' ||
+	die "unable to create authorization for /secret"
 
 echo
 echo "* Creating authorization for /topsecret"
@@ -66,7 +84,8 @@ a3sctl api create authorization -n "/testapp" \
 	--permissions '[
 		"/secret,GET",
 		"/topsecret,GET"
-	]'
+	]' ||
+	die "unable to create authorization for /topsecret"
 
 echo
 echo "* Success"
