ad781c6e9e8dfb8374c7474d40a7294821051e71
Antoine Mercadal <amercadal@paloaltonetworks.com>
2021-11-18 17:12:19 -0800

    a3sctl: add missing file

---
+++ cmd/a3sctl/authcmd/a3s.go
@@ -0,0 +1,60 @@
+package authcmd
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.aporeto.io/a3s/pkgs/authlib"
+	"go.aporeto.io/manipulate/manipcli"
+)
+
+func makeA3SCmd(mmaker manipcli.ManipulatorMaker) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:              "a3s",
+		Short:            "Use an A3S identity token.",
+		TraverseChildren: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			fToken := viper.GetString("access-token")
+			fAudience := viper.GetStringSlice("audience")
+			fCloak := viper.GetStringSlice("cloak")
+			fQRCode := viper.GetBool("qrcode")
+			fRestrictedPermissions := viper.GetStringSlice("restricted-permissions")
+			fRestrictedNetworks := viper.GetStringSlice("restricted-networks")
+			fRestrictedNamespace := viper.GetString("restricted-namespace")
+
+			if fToken == "" {
+				fToken = viper.GetString("token")
+			}
+
+			m, err := mmaker()
+			if err != nil {
+				return err
+			}
+
+			client := authlib.NewClient(m)
+			t, err := client.AuthFromToken(
+				context.Background(),
+				fToken,
+				authlib.OptAudience(fAudience...),
+				authlib.OptCloak(fCloak...),
+				authlib.OptRestrictNamespace(fRestrictedNamespace),
+				authlib.OptRestrictPermissions(fRestrictedPermissions),
+				authlib.OptRestrictNetworks(fRestrictedNetworks),
+			)
+			if err != nil {
+				return err
+			}
+
+			printToken(t, fQRCode)
+
+			return nil
+		},
+	}
+
+	cmd.Flags().String("access-token", "", "Valid Azure token.")
+
+	return cmd
+}
