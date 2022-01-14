cf4f2be1bfa065f3cb1489fd3e045253b59a5ae7
Yuheng Ouyang <govizlora@gmail.com>
2022-01-14 15:22:36 -0800

    ui/login: support qr code generation

--- ui/login/src/qr-code-dialog.tsx
+++ ui/login/src/qr-code-dialog.tsx
@@ -2,22 +2,62 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
-  DialogContentText,
   DialogActions,
   Button,
 } from "@mui/material"
+import { useState } from "react"
+import QrCode from "./qr-code-gen"
 
-export const QrCodeDialog = ({ token, onClose }: { token: string, onClose(): void }) => {
+export const QrCodeDialog = ({
+  token,
+  onClose,
+}: {
+  token: string
+  onClose(): void
+}) => {
+  const [imageDataUrl, setImageDataUrl] = useState("")
+
   return (
-    <Dialog open={!!token}>
-      <DialogTitle>QR Code</DialogTitle>
+    <Dialog open>
+      <DialogTitle>Token QR Code</DialogTitle>
       <DialogContent>
-        <DialogContentText>Below is your QR Code</DialogContentText>
-        {token}
+        <canvas
+          ref={canvas => {
+            if (canvas) {
+              const dpi = window.devicePixelRatio
+              const qr = QrCode.encodeText(token, QrCode.Ecc.LOW)
+              const border = 3
+              const scale = 3
+              const width: number = (qr.size + border * 2) * scale
+              canvas.width = width * dpi
+              canvas.height = width * dpi
+              canvas.style.width = `${width}px`
+              canvas.style.height = `${width}px`
+              let ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+              ctx.scale(dpi, dpi)
+              for (let y = -border; y < qr.size + border; y++) {
+                for (let x = -border; x < qr.size + border; x++) {
+                  ctx.fillStyle = qr.getModule(x, y) ? "#000000" : "#FFFFFF"
+                  ctx.fillRect(
+                    (x + border) * scale,
+                    (y + border) * scale,
+                    scale,
+                    scale
+                  )
+                }
+              }
+              setImageDataUrl(canvas.toDataURL("image/png"))
+            }
+          }}
+        />
       </DialogContent>
       <DialogActions>
-        <Button variant="contained" autoFocus>Save Image</Button>
-        <Button variant="outlined" onClick={onClose}>Close</Button>
+        <Button variant="contained" component='a' href={imageDataUrl} download={`token.png`} sx={{mr: 1}} >
+          Save Image
+        </Button>
+        <Button variant="outlined" onClick={onClose}>
+          Close
+        </Button>
       </DialogActions>
     </Dialog>
   )
