9ab4b01c0ecb929f1a1178312ab9d9fcd8bbccdf
Antoine Mercadal <amercadal@paloaltonetworks.com>
2022-01-20 15:42:38 -0800

    new: added unit tests for MakeTLSVerifyPeerCertificates

---
+++ pkgs/gwutils/gwutils_test.go
@@ -0,0 +1,149 @@
+package gwutils
+
+import (
+	"crypto"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"go.aporeto.io/a3s/pkgs/api"
+	"go.aporeto.io/elemental"
+	"go.aporeto.io/manipulate"
+	"go.aporeto.io/manipulate/maniptest"
+	"go.aporeto.io/tg/tglib"
+)
+
+func getECCert() (*x509.Certificate, crypto.PrivateKey) {
+
+	certBlock, keyBlock, err := tglib.Issue(pkix.Name{})
+	if err != nil {
+		panic(err)
+	}
+
+	cert, err := tglib.ParseCertificate(pem.EncodeToMemory(certBlock))
+	if err != nil {
+		panic(err)
+	}
+
+	key, err := tglib.PEMToKey(keyBlock)
+	if err != nil {
+		panic(err)
+	}
+
+	return cert, key
+}
+
+func TestMakeTLSVerifyPeerCertificate(t *testing.T) {
+
+	Convey("Given a cert and a manipulator", t, func() {
+
+		cert, _ := getECCert()
+		block, _ := tglib.CertToPEM(cert)
+		wrongCert, _ := getECCert()
+		wrongBlock, _ := tglib.CertToPEM(wrongCert)
+
+		m := maniptest.NewTestManipulator()
+
+		verifier := MakeTLSVerifyPeerCertificate(m, 10, time.Second)
+
+		Convey("When no cert are passed, nothing should happen", func() {
+			err := verifier(nil, nil)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("When given certs are garbage, it should fail", func() {
+			err := verifier([][]byte{[]byte("garbage")}, nil)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "tls: failed to parse certificate from server: x509: malformed certificate")
+		})
+
+		Convey("When the manipulator returns an error", func() {
+
+			m.MockRetrieveMany(t, func(mctx manipulate.Context, dest elemental.Identifiables) error {
+				return fmt.Errorf("boom")
+			})
+
+			err := verifier([][]byte{block.Bytes}, nil)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "unable to retrieve mtlssources: boom")
+		})
+
+		Convey("Calling with a certificate that matches an exising mtls source should work", func() {
+
+			called := 0
+			m.MockRetrieveMany(t, func(mctx manipulate.Context, dest elemental.Identifiables) error {
+				called++
+				*dest.(*api.MTLSSourcesList) = append(
+					*dest.(*api.MTLSSourcesList),
+					&api.MTLSSource{
+						CA: string(pem.EncodeToMemory(block)),
+					},
+				)
+				return nil
+			})
+
+			err := verifier([][]byte{block.Bytes}, nil)
+			So(err, ShouldBeNil)
+			So(called, ShouldEqual, 1)
+
+			Convey("When I call it again, it should work from the cache", func() {
+				err := verifier([][]byte{block.Bytes}, nil)
+				So(err, ShouldBeNil)
+				So(called, ShouldEqual, 1)
+			})
+		})
+
+		Convey("Calling with a certificate that matches an exising mtls with a wrong cert should fail", func() {
+
+			m.MockRetrieveMany(t, func(mctx manipulate.Context, dest elemental.Identifiables) error {
+				*dest.(*api.MTLSSourcesList) = append(
+					*dest.(*api.MTLSSourcesList),
+					&api.MTLSSource{
+						CA: string(pem.EncodeToMemory(wrongBlock)),
+					},
+				)
+				return nil
+			})
+
+			err := verifier([][]byte{block.Bytes}, nil)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldStartWith, "unable to validate client certificate: x509: certificate signed by unknown authority")
+		})
+
+		Convey("Calling with a certificate that does not matche any exising mtls source should fail", func() {
+
+			m.MockRetrieveMany(t, func(mctx manipulate.Context, dest elemental.Identifiables) error {
+				return nil
+			})
+
+			err := verifier([][]byte{block.Bytes}, nil)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "unable to retrieve any matching mtlssource")
+		})
+
+		Convey("When more than one source match, it should fail", func() {
+
+			m.MockRetrieveMany(t, func(mctx manipulate.Context, dest elemental.Identifiables) error {
+
+				*dest.(*api.MTLSSourcesList) = append(
+					*dest.(*api.MTLSSourcesList),
+					&api.MTLSSource{
+						CA: string(pem.EncodeToMemory(block)),
+					},
+					&api.MTLSSource{
+						CA: string(pem.EncodeToMemory(block)),
+					},
+				)
+				return nil
+			})
+
+			err := verifier([][]byte{block.Bytes}, nil)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "more than one mtls sources hold the signing CA. this is not supported")
+		})
+	})
+}
