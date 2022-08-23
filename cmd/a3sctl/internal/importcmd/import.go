edbe4126cb38bc2d4a800a3d1863f974a31dfd94
Antoine Mercadal <amercadal@paloaltonetworks.com>
2022-08-23 16:27:15 -0700

    a3sctl: classic forgot to save file

--- cmd/a3sctl/internal/importcmd/import.go
+++ cmd/a3sctl/internal/importcmd/import.go
@@ -31,7 +31,7 @@ func MakeImportCmd(mmaker manipcli.ManipulatorMaker) *cobra.Command {
 				viper.GetString("auto-auth-method"),
 				viper.GetStringSlice("audience"),
 				viper.GetStringSlice("cloak"),
-				viper.GetBool("renew"),
+				viper.GetBool("refresh"),
 				true,
 			); err != nil {
 				return err
