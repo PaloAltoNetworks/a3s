4ddaa2ffccba4bd0cb36a573931f9d051462e885
primalmotion <208711+primalmotion@users.noreply.github.com>
2022-10-04 10:44:06 -0700

    pkgs/nscache: added option system + OptionNotificationName (#61)
    * pkgs/nscache: added option system + OptionNotificationName
    
    * typo

---
+++ pkgs/nscache/options.go
@@ -0,0 +1,23 @@
+package nscache
+
+type config struct {
+	notificationName string
+}
+
+func newConfig() config {
+	return config{
+		notificationName: NotificationNamespaceChanges,
+	}
+}
+
+// Option represents a parametric option for the nscache.
+type Option func(*config)
+
+// OptionNotificationName allows to change the notification name
+// used to listen to namespace changes from the pubsub.
+// This defaults to NotificationNamespaceChanges,
+func OptionNotificationName(name string) Option {
+	return func(c *config) {
+		c.notificationName = name
+	}
+}
